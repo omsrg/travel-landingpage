6c04fdb98b0ea6821ffd08ccb00d2e79625d8efd
omsrg <siregar.om@gmail.com>
2022-02-21 18:33:20 +0700

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		fontFamily: {
+			raleway: ['Raleway', 'sans-serif'],
+			opensans: ['Open Sans', 'sans-serif'],
+		},
+		colors: {
+			primary: {
+				400: '#ffbf85',
+				500: '#ffa451',
+			},
+			secondary: {
+				400: '#355dab',
+				500: '#1f4590',
+			},
+			gray: {
+				400: '#535353',
+				500: '#4a4a4a',
+			},
+			dark: '#292929',
+		},
+		extend: {},
+	},
+	plugins: [],
+};
