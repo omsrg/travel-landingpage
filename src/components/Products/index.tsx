ffbeb254d8208609236ca6177af29e795f5e89d0
omsrg <siregar.om@gmail.com>
2022-02-23 22:24:03 +0700

    added swiper to products components

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -3,6 +3,12 @@ import Section from '../Section';
 import SectionHeading from '../section-heading';
 import ProductCard from './ProductCard';
 import ProductsItems from './ProductsItems';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination } from 'swiper';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/autoplay';
+import 'swiper/css/pagination';
 
 export default function Products() {
 	return (
@@ -19,29 +25,84 @@ export default function Products() {
 				</div>
 
 				<ProductsItems>
-					<ProductCard
-						category='Beach'
-						price={300}
-						location='Kuta, Indonesia'
-						description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta adipisci qui
+					<Swiper
+						modules={[Autoplay, Pagination]}
+						spaceBetween={30}
+						slidesPerView={3}
+						autoplay={{
+							delay: 2500,
+							disableOnInteraction: false,
+						}}
+						pagination={{
+							clickable: true,
+						}}
+					>
+						<SwiperSlide>
+							<ProductCard
+								category='Beach'
+								price={100}
+								location='Kuta, Indonesia'
+								description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta adipisci qui
 								nesciunt totam vel nihil id est corporis voluptatum sunt.'
-					/>
+							/>
+						</SwiperSlide>
 
-					<ProductCard
-						category='Building'
-						price={250}
-						location='Bangkok, Thailand'
-						description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta adipisci qui
+						<SwiperSlide>
+							<ProductCard
+								category='Building'
+								price={250}
+								location='Bangkok, Thailand'
+								description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta adipisci qui
 								nesciunt totam vel nihil id est corporis voluptatum sunt.'
-					/>
+							/>
+						</SwiperSlide>
 
-					<ProductCard
-						category='Beach'
-						price={300}
-						location='Yogyakarta, Indonesia'
-						description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta adipisci qui
+						<SwiperSlide>
+							<ProductCard
+								category='Beach'
+								price={300}
+								location='Yogyakarta, Indonesia'
+								description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta adipisci qui
 								nesciunt totam vel nihil id est corporis voluptatum sunt.'
-					/>
+							/>
+						</SwiperSlide>
+						<SwiperSlide>
+							<ProductCard
+								category='Beach'
+								price={400}
+								location='Yogyakarta, Indonesia'
+								description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta adipisci qui
+								nesciunt totam vel nihil id est corporis voluptatum sunt.'
+							/>
+						</SwiperSlide>
+						<SwiperSlide>
+							<ProductCard
+								category='Beach'
+								price={500}
+								location='Yogyakarta, Indonesia'
+								description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta adipisci qui
+								nesciunt totam vel nihil id est corporis voluptatum sunt.'
+							/>
+						</SwiperSlide>
+						<SwiperSlide>
+							<ProductCard
+								category='Beach'
+								price={600}
+								location='Yogyakarta, Indonesia'
+								description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta adipisci qui
+								nesciunt totam vel nihil id est corporis voluptatum sunt.'
+							/>
+						</SwiperSlide>
+						<SwiperSlide>
+							<ProductCard
+								category='Beach'
+								price={700}
+								location='Yogyakarta, Indonesia'
+								description='Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta adipisci qui
+								nesciunt totam vel nihil id est corporis voluptatum sunt.'
+							/>
+						</SwiperSlide>
+					</Swiper>
 				</ProductsItems>
 			</div>
 		</Section>
--- src/components/hero/form-input/input-date.tsx
+++ src/components/hero/form-input/input-date.tsx
@@ -2,19 +2,15 @@ import * as React from 'react';
 import { BsFillCalendarFill } from 'react-icons/bs';
 
 export default function InputDate() {
-	const today = new Date();
-	const date = today.setDate(today.getDate());
-	const defaultValue = new Date(date).toISOString().split('T')[0];
+	// const today = new Date();
+	// const date = today.setDate(today.getDate());
+	// const defaultValue = new Date(date).toISOString().split('T')[0];
 	return (
 		<div className='relative flex items-center space-x-4'>
 			{/* <BsFillCalendarFill className='w-6 h-6 text-secondary-500' /> */}
 			<div className='date'>
 				<p className='mb-1 text-tundora-400'>Date</p>
-				<input
-					type='date'
-					defaultValue={defaultValue}
-					className='px-2 py-1 w-max font-bold rounded-md focus:ring-0 border-0'
-				/>
+				<input type='date' className='px-2 py-1 w-max font-bold rounded-md focus:ring-0 border-0' />
 			</div>
 		</div>
 	);
