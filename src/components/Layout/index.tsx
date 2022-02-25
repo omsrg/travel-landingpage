55e11407e797f8866153b4f1c6c5c4178be28b3a
omsrg <siregar.om@gmail.com>
2022-02-25 14:59:46 +0700

    renamed header

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Header from '../Header';
+import Header from '../Header/Header';
 import Footer from '../Footer';
 import MobileNavigation from '../Header/MobileNavigation';
 
