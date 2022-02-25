c36aae0199baad2356170b224a4b4bc9927b1ccb
omsrg <siregar.om@gmail.com>
2022-02-25 15:14:20 +0700

    failed deploy

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import Header from '../Header/Header';
 import Footer from '../Footer';
+import Header from '../Header/Header';
 import MobileNavigation from '../Header/MobileNavigation';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
